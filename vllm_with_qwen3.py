58ab9699d95e54c72f1455c07e450be186d4da53
samwell <35051779+Samwellwang@users.noreply.github.com>
2025-06-26 11:54:00 +0800

    Update vllm_with_qwen3.py

--- vllm_with_qwen3.py
+++ vllm_with_qwen3.py
@@ -31,29 +31,6 @@ def content_ner_with_llm(content):
         model_name = "/root/.cache/huggingface/Qwen3-8B"
         result = request_vllm_model_with_openai_client(llm_host, prompt_str + content, model_name,
                                                        schema=NERInfo.model_json_schema())
-        if result:
-            try:
-                data = result[result.find("{"):result.rfind("}") + 1]
-                data = json.loads(data)
-                #判断 返回格式是否是list
-                for key in ['姓名', '联系方式', '地址', '工作单位', '职位', '邮箱', '所属部门', '职称', '传真',
-                            '个人主页', '社交信息']:
-                    key_value_list = data.get(key, None)
-                    if not key_value_list:
-                        continue
-                    if type(key_value_list) is not list:
-                        del data[key]
-                        continue
-                    tmp_key_value_list = []
-                    for key_value in key_value_list:
-                        if type(key_value) is str:
-                            tmp_key_value_list.append(key_value)
-                        if type(key_value) is dict:
-                            tmp_key_value_list.append(json.dumps(key_value))
-                    data[key] = tmp_key_value_list
-                return data if data else None
-            except Exception as e:
-                traceback.format_exc()
     return None
 
 
@@ -97,6 +74,6 @@ def request_vllm_model_with_openai_client(host_llm, prompt, model_name, image_pa
 
 
 if __name__ == '__main__':
-    os.environ["llm_host"] = "http://xxxx:8011/v1/"
+    os.environ["llm_host"] = "http://192.168.205.142:8011/v1/"
     content_ner_with_llm(
         "我样样事总喜欢革新；我虽然没做过国民党党员，可是我们一家人和革命的关系很深。我后来到了够岁数的时候还加入了国民党前身的同盟会(不是我自己加入的，是林贯虹弟兄在日本给我加入的)。我祖父根本就不是大清帝国的一个忠实的老百姓，所以他一生不愿考科举或做官，以后出洋回来保举也不愿接受。他除了因为感情生活上失望以外(参看第十六章)，他的革命思想也是他出洋回来不做官的一个理由。有一年夏天的晚上我差不多七岁的时候，祖父和父亲站在槅子门边谈英国的宪法和人权的事。我一点不懂，惟说到人民有权选举等等事，我觉得非常有意思。(其实我也不知是什么，不过我一小就觉得什么事可以由我做点主总是好的。只要别人叫我做什么，我总问为什么你要我做这个、做那个呢？)我就在旁边问什么叫人民有权？权是什么？父亲回我，又多嘴了，没有规矩！说完了笑笑。因为那时中国家庭规矩长辈说话，小孩子们不能插嘴的，不管是非好奇也不能问的，须等说完以后，才可以小声问一下。若是长辈不愿解说就完了，也不能再追问。但是我的祖父和父亲非常讲新法的人，我本人自己又是一小惯的不得了，所以问时父亲常解说给我听，祖父更喜欢人多问，所以养成我“打破沙锅璺到底，还问沙锅怎么起”的习惯来了。父亲骂我多嘴是照规矩，笑笑是表示以后再告诉我，免我失望，这样子把我越惯越没规矩。所以我到今天也学不会外国开会式的交际谈话，非得等一个人一串话说完了你才能说，等轮到我说时我早把我要说的话忘记了。并且碰到个贫嘴的人你不打他的岔怎么止得住他呢？")
